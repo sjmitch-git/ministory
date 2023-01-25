ba3d1e72d53017671f6e5cc50abc1a0ad1d65af9
sjmitch-git <sjmitch20@outlook.com>
2023-01-25 21:11:12 +0000

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import '../styles/index.css'
 export default function App({ Component, pageProps }) {
 
 	return (
-		<Component {...pageProps} />
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
 	)
 }
