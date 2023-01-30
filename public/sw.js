f37ceee9078d1d119cf5a7b43fda9ad98da9336b
sjmitch-git <sjmitch20@outlook.com>
2023-01-30 15:50:54 +0000

    PWA

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,27 @@
 if (!self.define) {
 	let e,
 		n = {}
-	const c = (c, s) => (
-		(c = new URL(c + '.js', s).href),
-		n[c] ||
+	const a = (a, s) => (
+		(a = new URL(a + '.js', s).href),
+		n[a] ||
 			new Promise((n) => {
 				if ('document' in self) {
 					const e = document.createElement('script')
-					;(e.src = c), (e.onload = n), document.head.appendChild(e)
-				} else (e = c), importScripts(c), n()
+					;(e.src = a), (e.onload = n), document.head.appendChild(e)
+				} else (e = a), importScripts(a), n()
 			}).then(() => {
-				let e = n[c]
-				if (!e) throw new Error(`Module ${c} didn’t register its module`)
+				let e = n[a]
+				if (!e) throw new Error(`Module ${a} didn’t register its module`)
 				return e
 			})
 	)
-	self.define = (s, a) => {
-		const i = e || ('document' in self ? document.currentScript.src : '') || location.href
-		if (n[i]) return
+	self.define = (s, i) => {
+		const c = e || ('document' in self ? document.currentScript.src : '') || location.href
+		if (n[c]) return
 		let o = {}
-		const r = (e) => c(e, i),
-			t = { module: { uri: i }, exports: o, require: r }
-		n[i] = Promise.all(s.map((e) => t[e] || r(e))).then((e) => (a(...e), o))
+		const r = (e) => a(e, c),
+			t = { module: { uri: c }, exports: o, require: r }
+		n[c] = Promise.all(s.map((e) => t[e] || r(e))).then((e) => (i(...e), o))
 	}
 }
 define(['./workbox-588899ac'], function (e) {
@@ -33,53 +33,54 @@ define(['./workbox-588899ac'], function (e) {
 		e.clientsClaim(),
 		e.precacheAndRoute(
 			[
+				{
+					url: '/_next/static/VwCmyid6tD3jziZeH4FvI/_buildManifest.js',
+					revision: 'b37a72060c8494d2c5dbb3aa3275fe1b',
+				},
+				{
+					url: '/_next/static/VwCmyid6tD3jziZeH4FvI/_ssgManifest.js',
+					revision: 'b6652df95db52feb4daf4eca35380933',
+				},
 				{ url: '/_next/static/chunks/framework-114634acb84f8baa.js', revision: '114634acb84f8baa' },
 				{ url: '/_next/static/chunks/main-8e65c6fcee628a80.js', revision: '8e65c6fcee628a80' },
-				{ url: '/_next/static/chunks/pages/_app-5d5fa813788ae392.js', revision: '5d5fa813788ae392' },
+				{ url: '/_next/static/chunks/pages/_app-d6576ad0670a65bb.js', revision: 'd6576ad0670a65bb' },
 				{ url: '/_next/static/chunks/pages/_error-8353112a01355ec2.js', revision: '8353112a01355ec2' },
-				{ url: '/_next/static/chunks/pages/index-8738359cc1644228.js', revision: '8738359cc1644228' },
+				{ url: '/_next/static/chunks/pages/index-b5cac7d9c6d9627d.js', revision: 'b5cac7d9c6d9627d' },
 				{
 					url: '/_next/static/chunks/polyfills-c67a75d1b6f99dc8.js',
 					revision: '837c0df77fd5009c9e46d446188ecfd0',
 				},
 				{ url: '/_next/static/chunks/webpack-ee7e63bc15b31913.js', revision: 'ee7e63bc15b31913' },
-				{ url: '/_next/static/css/45c05840da31d4fa.css', revision: '45c05840da31d4fa' },
-				{
-					url: '/_next/static/q5xhS-J060YTsfsfK_IMv/_buildManifest.js',
-					revision: '7e49c86b4117b45318c836e0ceb12147',
-				},
-				{
-					url: '/_next/static/q5xhS-J060YTsfsfK_IMv/_ssgManifest.js',
-					revision: 'b6652df95db52feb4daf4eca35380933',
-				},
-				{ url: '/android-icon-144x144.png', revision: '9ef226bbe167af1e76183464969ff8a1' },
-				{ url: '/android-icon-192x192.png', revision: 'ffedba395effe2255c3c406b7898a654' },
-				{ url: '/android-icon-36x36.png', revision: '91ccffbf312b64d62cc806008258f99e' },
-				{ url: '/android-icon-48x48.png', revision: '028e0ba3986fd702a5ec4c1e27693fe2' },
-				{ url: '/android-icon-72x72.png', revision: '72d78f4c3738d02bf92f705bc8d378ce' },
-				{ url: '/android-icon-96x96.png', revision: '32cbb70ac1578cdfb809bc6f1a4b104c' },
-				{ url: '/apple-icon-114x114.png', revision: 'ca44ab4eda3c0d45f2e54cc9f459a48c' },
-				{ url: '/apple-icon-120x120.png', revision: 'a04048eec039acbb6ed4bc7d13dfa3e4' },
-				{ url: '/apple-icon-144x144.png', revision: '9ef226bbe167af1e76183464969ff8a1' },
-				{ url: '/apple-icon-152x152.png', revision: '049de8a142d6e347176aaac968347c13' },
-				{ url: '/apple-icon-180x180.png', revision: 'a14f7625ffde2619724e642941b5a85c' },
-				{ url: '/apple-icon-57x57.png', revision: 'bff23945c94c92e40fde710bfe48d639' },
-				{ url: '/apple-icon-60x60.png', revision: '565d4adfb6cdd9d0dcee02305fb08808' },
-				{ url: '/apple-icon-72x72.png', revision: '72d78f4c3738d02bf92f705bc8d378ce' },
-				{ url: '/apple-icon-76x76.png', revision: '35ff4cecd8a8bd054470f447872d4a86' },
-				{ url: '/apple-icon-precomposed.png', revision: 'c6f75cca261b9edb7afccf1cccf97f6b' },
-				{ url: '/apple-icon.png', revision: 'c6f75cca261b9edb7afccf1cccf97f6b' },
-				{ url: '/brand.png', revision: 'f9f78f137e8144bab11a89a41e6a4fc2' },
+				{ url: '/_next/static/css/55931f25953632a2.css', revision: '55931f25953632a2' },
+				{ url: '/android-icon-144x144.png', revision: '5a8828c13a40008208b27060e912248f' },
+				{ url: '/android-icon-192x192.png', revision: '29bf651dba152f7059e90569244ee826' },
+				{ url: '/android-icon-36x36.png', revision: 'de6e810b6967d73fca5d93753a6ea671' },
+				{ url: '/android-icon-48x48.png', revision: '77488ced543c82b7fee63f5dbda347f8' },
+				{ url: '/android-icon-72x72.png', revision: 'd15495160c183a5f7f8f33cd9572c17b' },
+				{ url: '/android-icon-96x96.png', revision: '4902d9190576a667aa8cd3320baf52a4' },
+				{ url: '/apple-icon-114x114.png', revision: '531f300532154993a69bea6735892c00' },
+				{ url: '/apple-icon-120x120.png', revision: '7aa2f13fcfac8326af22a739ffe6e98a' },
+				{ url: '/apple-icon-144x144.png', revision: 'beaa1d286651e526cc855280dcf1a373' },
+				{ url: '/apple-icon-152x152.png', revision: '5e701dac2d059a861464161f4330068f' },
+				{ url: '/apple-icon-180x180.png', revision: '93714ba2fb5acc6a5879fbac983faed5' },
+				{ url: '/apple-icon-57x57.png', revision: '02ca7fa30ac1099532484c3da2ddd28f' },
+				{ url: '/apple-icon-60x60.png', revision: 'c8f383d900be031c8f8c817c4ebb370d' },
+				{ url: '/apple-icon-72x72.png', revision: '8da25699637c62df2899ecd82c6cd775' },
+				{ url: '/apple-icon-76x76.png', revision: '22e730d08d6f7f7fcb921b8b28ab1a9b' },
+				{ url: '/apple-icon-precomposed.png', revision: 'e828c073d4067d133dd64d8a2f6211e2' },
+				{ url: '/apple-icon.png', revision: 'e828c073d4067d133dd64d8a2f6211e2' },
+				{ url: '/brand.png', revision: 'bbe358752222db2e95ce8c137f3004d7' },
 				{ url: '/browserconfig.xml', revision: '653d077300a12f09a69caeea7a8947f8' },
-				{ url: '/favicon-16x16.png', revision: 'ca4341df77c0828a45c964f0412a7ac5' },
-				{ url: '/favicon-32x32.png', revision: '382e0e36808f95528696befcc6be003d' },
-				{ url: '/favicon-96x96.png', revision: '32cbb70ac1578cdfb809bc6f1a4b104c' },
-				{ url: '/favicon.ico', revision: 'e7fade86b02c758147f4e60dd29d59e3' },
-				{ url: '/manifest.json', revision: '9a6436541c423f45936ca140ae9eea1d' },
-				{ url: '/ms-icon-144x144.png', revision: '9ef226bbe167af1e76183464969ff8a1' },
-				{ url: '/ms-icon-150x150.png', revision: 'fe0d421efe6959cbcba4f7acf3be17dd' },
-				{ url: '/ms-icon-310x310.png', revision: 'd85e4c227cd571cf6d90c399a7d8a3b5' },
-				{ url: '/ms-icon-70x70.png', revision: '23dde6820d10804f91cd25d5097557e4' },
+				{ url: '/favicon-16x16.png', revision: '656ed179570295c994dc4e13fe01eff6' },
+				{ url: '/favicon-32x32.png', revision: '03d493aeffc12d879d9113c9286176c1' },
+				{ url: '/favicon-96x96.png', revision: '468b883f31abd66a1333f8b93fc986d7' },
+				{ url: '/favicon.ico', revision: '8923a98c77b371058289dbd12600012b' },
+				{ url: '/manifest.json', revision: 'b58fcfa7628c9205cb11a1b2c3e8f99a' },
+				{ url: '/ms-icon-144x144.png', revision: 'beaa1d286651e526cc855280dcf1a373' },
+				{ url: '/ms-icon-150x150.png', revision: '7089605a01cb1dcd4d4b9b620e7f6120' },
+				{ url: '/ms-icon-310x310.png', revision: '46f6c0ae8b30a372848cc0114c272545' },
+				{ url: '/ms-icon-70x70.png', revision: '36e425cfedefa7c32c5ba018e99c14c7' },
+				{ url: '/robots.txt', revision: '63e6aba7ddb50e8282c5cfecf017b9ff' },
 			],
 			{ ignoreURLParametersMatching: [] }
 		),
@@ -90,7 +91,7 @@ define(['./workbox-588899ac'], function (e) {
 				cacheName: 'start-url',
 				plugins: [
 					{
-						cacheWillUpdate: async ({ request: e, response: n, event: c, state: s }) =>
+						cacheWillUpdate: async ({ request: e, response: n, event: a, state: s }) =>
 							n && 'opaqueredirect' === n.type
 								? new Response(n.body, { status: 200, statusText: 'OK', headers: n.headers })
 								: n,
