ea81b8a4ea55af93db9b88ff15d8e9aee97e655f
sjmitch-git <sjmitch20@outlook.com>
2023-03-04 12:06:37 +0000

    tab link colour

--- components/results/Results.jsx
+++ components/results/Results.jsx
@@ -11,23 +11,6 @@ const Results = () => {
 			className='mb-8 flex flex-col-reverse gap-16'
 			id='results'
 			data-testid='results'>
-			{/* 			{results &&
-				results.map((obj, index) => (
-					<div
-						className='max-w-3xl px-2 md:px-4 '
-						key={index}>
-						<p
-							className='py-6 text-3xl font-bold uppercase tracking-wide opacity-60 md:text-4xl'
-							data-testid='label'>
-							{obj.label}
-						</p>
-						<span
-							data-testid='data'
-							className='font-mono text-lg tracking-widest opacity-80 md:text-xl lg:text-2xl'>
-							{obj.data}
-						</span>
-					</div>
-				))} */}
 			{results &&
 				results.map((obj, index) => (
 					<Story
